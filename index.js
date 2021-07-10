0142c48c18a8ac2e3ec058c0684f315c17bfce8e
Nathan Hedge <23344786+10Nates@users.noreply.github.com>
2021-07-10 09:47:57 -0500

    yeah

--- index.js
+++ index.js
@@ -1,7 +1,53 @@
-fetch('https://api.waifu.pics/sfw/waifu').then(res => {
-    res.blob().then(b => {
-        b.text().then(t => {
-            window.location.href = JSON.parse(t).url
+const http = require('http')
+const https = require('https');
+
+function newImageURL(callback) {
+    https.get('https://api.waifu.pics/sfw/waifu', (response) => {
+        res = '';
+        response.on('data', (chunk) => { res += chunk })
+
+        response.on('end', () => {
+            url = JSON.parse(res).url
+            callback(url)
         })
     })
-})
+}
+
+function newImageData(callback) {
+    newImageURL(url => {
+        https.get(url, (response) => {
+            var res
+            response.pipe(res)
+
+            response.on('end', () => {
+                callback([url, res])
+            })
+        })
+    })
+}
+
+
+var server = http.createServer(function (req, reso) {
+
+    newImageURL(url => {
+        https.get(url, (response) => {
+            var typef = url.split(/\./g)
+            typef = typef[typef.length - 1]
+            var type = 'image';
+            if (typef === 'png') type = 'image/png'
+            else if (typef.match(/jpg|jpeg|jfif/g)) type = 'image/jpeg'
+            else if (typef === 'webp') type = 'image/webp'
+
+            reso.writeHead(200, { 'Content-Type': type });
+            response.on('data', chunk => reso.write(chunk))
+
+            response.on('end', () => {
+                reso.end()
+            })
+        })
+    })
+
+}).listen(process.env.PORT || 5000);
+
+server.on('error', err => console.error(err))
+server.on('close', () => console.log('Get relay server closed'))
